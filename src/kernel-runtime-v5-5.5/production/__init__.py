8f6ace38b67c66435946405b95cb6203712c3670
qingfeng-yangyang <yangqingming95@gmail.com>
2026-07-13 20:40:11 +0800

    Delete src/kernel-runtime-v5-5.5 directory

--- src/kernel-runtime-v5-5.5/production/__init__.py
+++
@@ -1,4 +0,0 @@
-from .backend import InMemoryJobBackend, RedisJobBackend
-from .service import ProductionRuntimeService
-
-__all__ = ["InMemoryJobBackend", "RedisJobBackend", "ProductionRuntimeService"]
